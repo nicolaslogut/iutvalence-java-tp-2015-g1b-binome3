7c6795170fdb53de24600b0a2f47048fe561260f
dazyj <jeremie.dazy@iut-valence.fr>
2015-04-02 11:46:16 +0200

    initial commit

---
+++ src/Grille.java
@@ -0,0 +1,8 @@
+
+public class Grille 
+{
+	public final static int NB_LIGNES = 5;
+	public final static int NB_COLONNES = 5;
+	public final static int NB_LAMPES_GRILLE = NB_LIGNES * NB_COLONNES;
+	
+}
---
+++ src/Groupe.java
@@ -0,0 +1,13 @@
+
+public class Groupe 
+{
+	private String typeGroupe;
+	private int nbLampesAdjacentes;
+	
+	public Groupe(String nom, int nb)
+	{
+		this.typeGroupe = nom;
+		this.nbLampesAdjacentes = nb;		
+	}	
+	
+}
