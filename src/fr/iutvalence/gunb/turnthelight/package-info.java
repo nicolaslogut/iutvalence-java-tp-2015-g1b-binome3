c188745043c6085071d2e76eac3a43bb9428f10a
dazyj <jeremie.dazy@iut-valence.fr>
2015-04-29 16:44:06 +0200

    29/04/2015 commit

---
+++ src/fr/iutvalence/gunb/turnthelight/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * @author dazyj
+ * 
+ * package principal du projet Turn The Light, jeu de réflexion consistant à éteindre des lampes allumées.
+ * 
+ * Le package contient 4 classes.
+ *
+ */
+package fr.iutvalence.gunb.turnthelight;
